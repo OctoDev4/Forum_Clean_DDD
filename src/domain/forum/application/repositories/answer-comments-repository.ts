84bc93f1a717756c8c73f39a163949f795e840cc
Felipe Almeida <felipe.alhopato@gmail.com>
2024-04-17 23:40:29 -0300

    delete answer and question comments

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -2,5 +2,7 @@ import {AnswerComment} from "@/domain/forum/enterprise/entities/answer-comment";
 
 
 export interface AnswerCommentsRepository{
+    findById(id:string):Promise<AnswerComment | null>
     create(answerComment:AnswerComment): Promise<void>
+    delete(AnswerComment:AnswerComment): Promise<void>
 }
